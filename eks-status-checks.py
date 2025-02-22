5458adc22353bbe2c2f3bec044fce0a504589724
Prince450 <princetonabdulsalam25@gmail.com>
2025-02-22 17:31:44 -0500

    final

--- eks-status-checks.py
+++ eks-status-checks.py
@@ -1,22 +1,3 @@
-#  Automating displaying EKS cluster information
-
-In this project, I've put together a neat way to automatically fetch and d
-isplay information about an Amazon EKS (Elastic Kubernetes Service) cluster. 
-I used Python, along with the Boto3 library, to interact with AWS EKS and pulls out the details 
-about the cluster status, kubernetes version and cluster endpoint. 
-
-I also used Terraform to create the EKS Cluster itself.
-
-## What I Did
-
-1.  **EKS Cluster Setup:** I created the EKS cluster using Terraform.
-
-    ![eks](https://github.com/Princeton45/python-eks-monitor/blob/main/images/eks.png)
-
-2.  **Wrote the Python Script:** I developed a Python script that uses Boto3 to connect to AWS. The script grabs all the  details about the EKS cluster, 
-like its cluster status, kubernetes version and cluster endpoint.
-
-```python
 # Import the boto3 library, which is the AWS SDK for Python
 import boto3
 
@@ -58,6 +39,3 @@ for cluster in clusters:
     print(f"Cluster {cluster} status is  {cluster_status}")
     print(f"Cluster endpoint: {cluster_endpoint}")
     print(f"Cluster version: {cluster_version}")
-```
-
-![output](https://github.com/Princeton45/python-eks-monitor/blob/main/images/output.png)
